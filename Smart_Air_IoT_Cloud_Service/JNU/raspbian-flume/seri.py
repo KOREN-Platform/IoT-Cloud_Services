a411b2537efc160b3d5b7be09d9259ab17dbb4f0
Shane <tjdghks0531@gmail.com>
2017-11-16 17:59:08 +0900

    전남대 라즈베리파이쪽 코드 수정

--- Smart_Air_IoT_Cloud_Service/JNU/raspbian-flume/seri.py
+++ Smart_Air_IoT_Cloud_Service/JNU/raspbian-flume/seri.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-nullstr = 9999
+nullstr = -1
 id = '"RaspberryPi"'
 light = nullstr
 temp = nullstr
@@ -12,20 +12,25 @@ humidity = nullstr
 luminance = nullstr
 alcohol_gas = nullstr
 co_gas = nullstr
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 if(ser.isOpen() == False):
     ser.open()
 ser.readline()
 time.sleep(2)
 while 1:
         response = ser.readline()
-        if response :
-                temp = response.split(' ; ')[0]
-                a = response.split(' ; ')[1]
-                light = a
-                co_gas = response.split(' ; ')[2]
-                alcohol_gas = response.split(' ; ')[3]
-                dust = response.split(' ; ')[4]
+#	print(response.split(';'))
+#	if response!='\r\n' and response :
+	if len(response.split(';')) == 8 :
+                temp = response.split(';')[0]
+                light = response.split(';')[1]
+                co_gas = response.split(';')[2]
+                alcohol_gas = response.split(';')[3]
+                dust = response.split(';')[4]
+		latitude = response.split(';')[5]
+		longitude = response.split(';')[6]
                 f = open("/var/log/apache/flumeSpool/sensor.txt", 'a')
+		#f = open("/var/log/apache/flumeSpool/sensorlog", 'a')
                 data = '{ "ID" : %s , "light" : %s , "temp" : %s , "latitude" : %s , "longitude" : %s , "dust" : %s , "alcohol_gas" : %s , "co_gas" : %s }\n' %(id,light,temp,latitude,longitude,dust,alcohol_gas,co_gas)
                 f.write(data)
+	time.sleep(1)
